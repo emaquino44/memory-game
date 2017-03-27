a4926f2dff8eac53c0b07ab035abeea1ffcdfe8e
emaquino44 <prexy44@yahoo.com>
2017-03-27 10:09:30 -0700

    created game logic

---
+++ js/main.js
@@ -0,0 +1,56 @@
+
+var cards = [
+	{	
+		rack: 'queen',
+		suit: 'hearts',
+		cardImage: 'images/queen-of-hearts.png',
+	},
+	{
+		rack: 'queen',
+		suit: 'diamonds',
+		cardImage: 'images/queen-of-diamonds.png',
+	},
+	{
+		rack: "king",
+		suit: "hearts",
+		cardImage: "images/king-of-hearts.png"
+	},
+	{
+		rack: "king",
+		suit: "diamonds",
+		cardImage: "images/king-of-diamonds.png"
+	},
+];
+var cardsInPlay = [];
+
+
+
+var checkForMatch = function() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You've found a match") 
+	} else {
+		alert("Sorry, try again.");
+	}
+};
+var flipCard = function() {
+	cardId = this.getAttribute('data-id');
+	console.log(cardId);
+	cardsInPlay.push(cards[cardID].rank);
+	this.setAttribute('src', cards[cardsID].cardImage)
+	if(cardsInPlay.length === 2) {
+	checkForMatch();
+	cardsInPlay = [];
+	}
+};
+
+var createBoard = function() {
+	for (var i = 0; i < cards.length; i++) {
+ var cardElement = document.createElement('img');
+  cardElement.setAttribute('src', 'images/back.png');
+  cardElement.setAttribute('data-id', i);
+  cardElement.addEventListener('click', flipCard);
+  document.getElementById('game-board').appendChild(cardElement);
+	}
+}
+createBoard();
+
